6799798e0b3bbf21b002361dc86fcb67d686e22c
erwinislasegura <erwin.2785@gmail.com>
2026-06-30 02:44:27 -0400

    Quita subtitulo y estado del formulario recepcion MP

--- assest/documento/registroICarga.php
+++ assest/documento/registroICarga.php
@@ -2253,7 +2253,7 @@ if (isset($_POST)) {
         <script src="../../assest/js/multisteps.js"></script>
 </head>
 
-<body class="hold-transition light-skin fixed sidebar-mini theme-primary" >
+<body class="hold-transition light-skin fixed sidebar-mini theme-primary sistemRR" >
     <div class="wrapper">
         <!-- LLAMADA AL MENU PRINCIPAL DE LA PAGINA -->
             <?php include_once "../../assest/config/menuFruta.php";
